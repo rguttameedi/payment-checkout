3ee8bc19939c500bc65a0bb034cc199cb5be54ea
rguttameedi <Raju.guttameedi1@realpage.com>
2026-02-12 22:36:18 +0530

    Update server.js
    Fix port for Render deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const https = require('https');
 
 // Create Express app
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
